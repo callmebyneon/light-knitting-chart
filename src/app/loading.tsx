0ddfb41ec54e9c181924ff893ddad98a495e656c
YI Na-yeon <2n.ayeon.yi@gmail.com>
2026-07-01 14:32:48 +0900

    style: loader spinner 스타일 수정 => 전체화면

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,9 @@
 import { Spinner } from "@/components/ui/spinner";
 
-export default function loading() {
+export default function Loading() {
   return (
-    <div>
-      <Spinner className="size-8" />
+    <div className="h-screen w-full overflow-hidden text-sm flex items-center justify-center fixed top-0 left-0">
+      <Spinner className="size-8 text-[#00bcff]" />
     </div>
   );
 }
